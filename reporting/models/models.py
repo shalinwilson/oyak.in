3ae4066e8ee9acdef08d0af44fcb2de55387d484
jiju <jiju@gmail.com>
2024-06-03 16:41:52 +0530

    Ads

--- reporting/models/models.py
+++ reporting/models/models.py
@@ -2,27 +2,29 @@
 
 from odoo import models, fields, api
 
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     cost_for_reporting = fields.Integer()
 
+
 class reporting(models.Model):
     _name = 'reporting.reporting'
     _description = 'reporting.reporting'
 
-    name = fields.Char()
     date1 = fields.Datetime()
     date2 = fields.Datetime()
     sales_amount_collected = fields.Float()
     total_amount_refunded = fields.Float()
     shipping_cost_total = fields.Float()
     other_deductions = fields.Float()
+    profit_against_collected_amount = fields.Float()
 
     total_orders = fields.Integer()
     total_cod = fields.Integer()
     total_prepaid = fields.Integer()
-    cod_prepaid_ratio = fields.Float()
+    cod_prepaid_ratio = fields.Float("COD %")
     total_rto_orders = fields.Float()
     rto_loss = fields.Float()
     delivery_payments_total = fields.Float()
@@ -30,38 +32,37 @@ class reporting(models.Model):
 
     calculated_profit = fields.Float()
     note = fields.Char()
+
     def calculate_values(self):
         print("working")
         start_date = fields.Datetime.to_string(self.date1)
         end_date = fields.Datetime.to_string(self.date2)
         print(self.env['sale.order'].browse([1]).date_order)
-        so_in_period = self.env['sale.order'].search([('date_order','>=',start_date),
-                                                      ('date_order','<=',end_date),
-                                                      ('state','not in',['draft','sent','cancel'])
+        so_in_period = self.env['sale.order'].search([('date_order', '>=', start_date),
+                                                      ('date_order', '<=', end_date),
+                                                      ('state', 'not in', ['draft', 'sent', 'cancel'])
                                                       ])
         self.total_orders = len(so_in_period.filtered(lambda x: x.is_rto_order == False))
         self.total_cod = len(so_in_period.filtered(lambda self: self.payment_type == 'cod'))
         self.total_prepaid = len(so_in_period.filtered(lambda self: self.payment_type == 'Pre_paid'))
-        self.cod_prepaid_ratio = (self.total_prepaid / self.total_cod)
-        self.total_rto_orders = self.env['sale.order'].search_count([('date_order','>=',start_date),
-                                                      ('date_order','<=',end_date),
-                                                          ('is_rto_order','=',True),
-                                                      ])
-        self.rto_loss = sum(self.env['sale.order'].search([('date_order','>=',start_date),
-                                                      ('date_order','<=',end_date),
-                                                          ('is_rto_order','=',True)
-                                                      ]).mapped('delhivery_cost'))
-        delhivery_partner  = self.env['res.partner'].search([('name','=','Delhivery')],limit=1)
-        delhivery_payments = self.env['account.payment'].search([('date','>=',start_date),
-                                                      ('date','<=',end_date),
-                                                              ('state','=','posted'),
-                                                              ('partner_id','=',delhivery_partner.id),
-                                                              ('payment_type','=','outbound')
-                                                              ])
+        self.cod_prepaid_ratio = (self.total_cod / self.total_orders) * 100
+        self.total_rto_orders = self.env['sale.order'].search_count([('date_order', '>=', start_date),
+                                                                     ('date_order', '<=', end_date),
+                                                                     ('is_rto_order', '=', True),
+                                                                     ])
+        self.rto_loss = sum(self.env['sale.order'].search([('date_order', '>=', start_date),
+                                                           ('date_order', '<=', end_date),
+                                                           ('is_rto_order', '=', True)
+                                                           ]).mapped('delhivery_cost'))
+        delhivery_partner = self.env['res.partner'].search([('name', '=', 'Delhivery')], limit=1)
+        delhivery_payments = self.env['account.payment'].search([('date', '>=', start_date),
+                                                                 ('date', '<=', end_date),
+                                                                 ('state', '=', 'posted'),
+                                                                 ('partner_id', '=', delhivery_partner.id),
+                                                                 ('payment_type', '=', 'outbound')
+                                                                 ])
         self.note = str(delhivery_payments) + str(delhivery_partner)
         self.delivery_payments_total = sum(delhivery_payments.mapped('amount_total'))
-
-
 
         self.sales_amount_collected = sum(so_in_period.mapped('cod_collected'))
         self.shipping_cost_total = sum(so_in_period.mapped('delhivery_cost'))
@@ -75,16 +76,8 @@ class reporting(models.Model):
 
         self.products_total_cost = products_cost
 
-        self.calculated_profit = self.sales_amount_collected - (self.shipping_cost_total+self.total_amount_refunded
-                                                                +self.products_total_cost+self.other_deductions)
+        self.calculated_profit = self.sales_amount_collected - (self.shipping_cost_total + self.total_amount_refunded
+                                                                + self.products_total_cost + self.other_deductions)
 
-
-
-
-
-
-
-
-
-
-
+        self.profit_against_collected_amount = (self.calculated_profit / (
+                    self.sales_amount_collected - self.shipping_cost_total)) * 100
