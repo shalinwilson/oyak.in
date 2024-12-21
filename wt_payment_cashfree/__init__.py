566d2d6e4a9c61f60ad478426cf1d289e13cead3
jiju <jiju@gmail.com>
2024-12-22 00:49:16 +0530

    ASd'

--- wt_payment_cashfree/__init__.py
+++ wt_payment_cashfree/__init__.py
@@ -3,8 +3,3 @@ from . import controllers
 
 from odoo.addons.payment import setup_provider, reset_payment_provider
 
-def post_init_hook(env):
-	setup_provider(env, 'cashfree')
-
-def uninstall_hook(env):
-	reset_payment_provider(env, 'cashfree')
