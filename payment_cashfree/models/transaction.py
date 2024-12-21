55d492a232ca94c00fbc40b30deeb52b6e2f5efb
jiju <jiju@gmail.com>
2024-12-22 01:02:52 +0530

    Asd'

--- payment_cashfree/models/transaction.py
+++ payment_cashfree/models/transaction.py
@@ -24,6 +24,7 @@ class PaymentTransactionCashfree(models.Model):
 
     def _get_specific_rendering_values(self, values):
         _logger.info(str(values))
+        _logger.info("aas")
         res = super()._get_specific_rendering_values(values)
         if self.provider_code != 'cashfree':
             return res
@@ -41,7 +42,8 @@ class PaymentTransactionCashfree(models.Model):
         cashfree_values['signature'] = self.provider_id._cashfree_generate_sign('out', cashfree_values)
         values.update(cashfree_values)
         response = self.provider_id.get_cashfree_return_url(values)
-        _logger.info(str(response+"xxx"))
+        _logger.info(response)
+        _logger.info("response")
         cashfree_values.update({
             "paymentSessionId": response.get("payment_session_id"),
             "paymentMode": "production" if self.provider_id.state == 'enabled' else "sandbox",
